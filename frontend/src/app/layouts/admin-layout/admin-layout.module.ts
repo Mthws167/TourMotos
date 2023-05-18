93db8468692beb2825b045867da95f9ee2bdc6a2
Mthws167 <mthws.henrique@hotmail.com>
2023-05-18 09:31:18 -0300

    Alterando o frontend para melhor adaptação do projeto

--- frontend/src/app/layouts/admin-layout/admin-layout.module.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -6,11 +6,7 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
 import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatRippleModule} from '@angular/material/core';
@@ -35,11 +31,7 @@ import {MatSelectModule} from '@angular/material/select';
     DashboardComponent,
     UserProfileComponent,
     TableListComponent,
-    TypographyComponent,
-    IconsComponent,
     MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent,
   ]
 })
 
