cd1f735e56ea2ba545339d442cd9e608b70260cf
Mthws167 <mthws.henrique@hotmail.com>
2023-05-18 22:24:46 -0300

    Update na versão do angular para 16 e iniciando criação do mapa

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import {MapsComponent} from "./maps/maps.component";
 
 @NgModule({
   imports: [
--- frontend/src/app/layouts/admin-layout/admin-layout.module.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -13,6 +13,7 @@ import {MatRippleModule} from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatSelectModule} from '@angular/material/select';
+import {GoogleMapsModule} from "@angular/google-maps";
 
 @NgModule({
   imports: [
@@ -26,6 +27,7 @@ import {MatSelectModule} from '@angular/material/select';
     MatInputModule,
     MatSelectModule,
     MatTooltipModule,
+    GoogleMapsModule,
   ],
   declarations: [
     DashboardComponent,
